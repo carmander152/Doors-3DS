0638c6b4de65124e91af77bcd5febdf72524f067
carmander152 <carmander153@gmail.com>
2026-03-29 20:54:26 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -528,4 +528,5 @@ int main() {
         if(sWardrobeExit.data_vaddr)linearFree((void*)sWardrobeExit.data_vaddr); 
         ndspExit(); 
     }
-    romfsExit(); C3D_Fini(); gfxExit(); return 0;}
+    romfsExit(); C3D_Fini(); gfxExit(); return 0;
+}
