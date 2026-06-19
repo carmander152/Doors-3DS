c289fa79fa1f26edee82c3426f79ddd637f84802
carmander152 <carmander153@gmail.com>
2026-06-18 23:10:48 -0700

    Add MD2Model structure and methods in md2.h

---
+++ source/md2.h
@@ -0,0 +1,21 @@
+#ifndef MD2_H
+#define MD2_H
+
+#include <vector>
+#include "render_utils.h"
+
+struct MD2Model {
+    int numFrames;
+    int numVerts;
+    int numTris;
+    
+    std::vector<float> uvs; 
+    std::vector<int> triVerts; 
+    std::vector<int> triUVs;   
+    std::vector<std::vector<float>> frameVerts; 
+
+    bool load(const char* filepath);
+    void draw(int frame, float x, float y, float z, float scale, float L, float rotY);
+};
+
+#endif
