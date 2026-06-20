1c2461e809e63d5c934efff318fd3e45764db036
zerokizo <zerokizo@outlook.com>
2026-06-20 18:35:49 -0500

    that was the olny error in the build console i will rip you apart if you dont work

--- source/md2.cpp
+++ source/md2.cpp
@@ -8,7 +8,7 @@
 extern std::vector<vertex> seek_mesh;
 
 bool MD2Model::load(const char* filepath, const char* file_name) {
-    std::string full_path = std::string(path) + file_name;
+    std::string full_path = std::string(filepath) + file_name;
     FILE* file = fopen(full_path.c_str(), "rb");
     if (!file) return false;
 
